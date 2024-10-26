5fbcdab19d73664fe056aaa82b7fa1e0ac9b56b4
Mohammad Fakih <mohammadfakih025@gmail.com>
2024-10-26 15:48:11 +0300

    fixed python code

--- ex1.py
+++ ex1.py
@@ -1,11 +1,12 @@
 def palindrome_star(n):
     for i in range(1, n + 1):
-        print(" " * (n - i))
+        print(" " * (n - i),end="")
         print("* " * i)
         
     for i in range(n - 1, 0, -1):
-        print(" " * (n - i))
+        print(" " * (n - i),end="")
         print("* " * i)
 
-rows = int(input())
+
+rows = int(input(" enter number of rows "))
 palindrome_star(rows)
